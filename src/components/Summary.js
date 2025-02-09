28d9eb0a2f5ba6b63306dfb9cc19795f6e595e0e
Ramesh Meduri <rameshmeduri@outlook.com>
2025-02-09 11:18:43 +0530

    init

---
+++ src/components/Summary.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Summary = () => (
+  <section>
+
+    <div className="rs-green-text rs-mt-10">Professional Summary</div>
+    <ul className="rs-list">
+      <li className="rs-list-item"><code>Project Management</code> : Lead the full Project lifecycle, ensuring projects meet scope / budget / time / quality</li>
+      <li className="rs-list-item"><code>Change Management</code> : Manage changes to the Project ( scope / schedule / cost ) using verification techniques ( earned value management / critical path analysis / WBS )</li>
+      <li className="rs-list-item"><code>Risk Management</code> : Identify and mitigate Project risks, ensuring adherence to regulatory requirements and compliance standards</li>
+      <li className="rs-list-item"><code>Cost Management</code> : Estimating the costs of all resources required to complete the Project successfully</li>
+      <li className="rs-list-item"><code>Release Management</code> : successful roll-out of new features, bug fixes while maintaining system stability</li>
+      <li className="rs-list-item"><code>Project Metrics</code> : Monitoring Project performance using tools ( Earned Value Management / KPI's / Milestone Tracking / Quality Control Metrics )</li>
+      <li className="rs-list-item"><code>Problem Solving</code> : Identifying and resolving any project issues that may arise, including technical</li>
+      <li className="rs-list-item"><code>Agile Facilitator</code> : Facilitating Scrum Events such as Sprint Planning, Daily Stand-Ups, Sprint Reviews, Retrospectives</li>
+      <li className="rs-list-item"><code>Product Decisions</code> : Making decisions about the Product's Direction, Features, Roadmap</li>
+      <li className="rs-list-item"><code>Leadership & Interpersonal Skills</code> : ability to motivate, inspire Teams, build relationships and resolve conflicts</li>
+    </ul>
+    <div className="rs-green-text rs-mt-10">Tech Lead</div>
+    <div className="rs-pl-10">Versatile Technical Project Manager with 10+ years of experience, combining hands-on <code>React</code> development expertise with strong <code>Project Management</code> capabilities. Specialized in delivering <code>Enterprise Web Applications</code> for Banking and Financial Services, with 6+ years of Onsite experience in Singapore. Proven track record in leading full-stack <code>Javascript Projects</code> while managing cross-functional Teams using <code>Agile</code> Methodologies.</div>
+  </section>
+);
+
+export default Summary;
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { IconContext } from 'react-icons';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.scss';
+import './dark.css';
+
+
+import Header from './components/Header';
+import Summary from './components/Summary';
+// import Tags from './components/Tags';
+import SkillSet from './components/SkillSet';
+import Experience from './components/Experience';
+import Education from './components/Education';
+
+const Devider = () => <div className="rs-border-bottom" />;
+
+const App = () => {
+  return (
+    <IconContext.Provider value={{ color: '', className: 'global-class-name' }}>
+      <div className="container">        
+        <Header />
+        <Devider />
+        <Summary />
+        {/* <Tags /> */}
+        <SkillSet />
+        <Experience />
+        <Education />
+      </div>
+    </IconContext.Provider>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
